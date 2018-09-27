ef054577e604ca7f34a76af247cd5bfb30d4cb96
KlayChan <1358175850@qq.com>
2018-09-27 12:24:45 +0800

    QQTIM commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function ($) {
+    // 引入文件
+    // 结构化 HTML设置data-stellar-background-ratio="0.1" css 设置background-attachment: fixed;
+    // js 初始化
+    $.stellar({
+        horizontalScrolling: false,
+        responsive: true
+    });
+})
